56dcc04c49641d0f73492542f7b2708c75ce9e83
BlackRain <masaraksh86@mail.ru>
2022-04-27 18:56:49 +0700

    Создан проект "Console test"

---
+++ Console test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using YouTube_API;
+
+namespace Console_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
