c911e7bf1ec201ae5478ac15d1c1934cf83fea5e
BlackRain <masaraksh86@mail.ru>
2022-11-24 14:31:38 +0700

    Создан тест "TestChannelPages"

---
+++ YouTubeApiLib.TestChannelPages/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace YouTubeApiLib.TestChannelPages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            YouTubeChannel channel = new YouTubeChannel("UCSCHk4GbzMlKtxwpXPyYeMA", "Frozzen Fro");
+            YouTubeApi api = new YouTubeApi();
+            YouTubeApi.getMediaTracksInfoImmediately = false;
+            VideoIdPageResult videoIdPageResult = api.GetVideoIdPage(channel, null);
+            if (videoIdPageResult.ErrorCode == 200)
+            {
+                Console.WriteLine($"{channel.DisplayName} [{channel.Id}]: Videos tab page:");
+                foreach (string id in videoIdPageResult.VideoIdPage.VideoIds)
+                {
+                    Console.WriteLine(id);
+                }
+                Console.WriteLine($"Continuation token: {videoIdPageResult.VideoIdPage.ContinuationToken}");
+            }
+            else
+            {
+                Console.WriteLine($"{channel.DisplayName} [{channel.Id}]: Error code = {videoIdPageResult.ErrorCode}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
