1e60eb39aca16678b24a63b1004104b5ee495796
BlackRain <masaraksh86@mail.ru>
2022-12-03 14:42:56 +0700

    Обновлён тест "TestChannelPages"

--- YouTubeApiLib.TestChannelPages/Program.cs
+++ YouTubeApiLib.TestChannelPages/Program.cs
@@ -10,6 +10,7 @@ namespace YouTubeApiLib.TestChannelPages
             YouTubeChannel channel = new YouTubeChannel("UCSCHk4GbzMlKtxwpXPyYeMA", "Frozzen Fro");
             YouTubeApi api = new YouTubeApi();
             YouTubeApi.getMediaTracksInfoImmediately = false;
+            YouTubeApi.decryptMediaTrackUrlsAutomaticallyIfPossible = false;
             List<YouTubeChannelTabPage> pages = new List<YouTubeChannelTabPage>()
             {
                 YouTubeChannelTabPages.Videos,
@@ -24,7 +25,9 @@ namespace YouTubeApiLib.TestChannelPages
                     Console.WriteLine($"{channel} {page.Title} tab page:");
                     foreach (string id in videoIdPageResult.VideoIdPage.VideoIds)
                     {
-                        Console.WriteLine(id);
+                        YouTubeVideo video = api.GetVideo(new VideoId(id));
+                        string t = video != null ? $"{video.Id} > {video.Title}" : $"{id} > null";
+                        Console.WriteLine(t);
                     }
                     string token = !string.IsNullOrEmpty(videoIdPageResult.VideoIdPage.ContinuationToken) &&
                         !string.IsNullOrWhiteSpace(videoIdPageResult.VideoIdPage.ContinuationToken) ?
