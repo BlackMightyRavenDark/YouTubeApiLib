a4bfe8d5d8201d38a2922ecec34e434e9f026c3e
BlackRain <masaraksh86@mail.ru>
2023-05-21 18:55:02 +0700

    Исправлена опечатка

--- YouTubeApiLib/Utils.cs
+++ YouTubeApiLib/Utils.cs
@@ -761,13 +761,13 @@ namespace YouTubeApiLib
         }
 
         public static Dictionary<string, string> SplitStringToKeyValues(
-            string inputString, char keySeparaor, char valueSeparator)
+            string inputString, char keySeparator, char valueSeparator)
         {
             if (string.IsNullOrEmpty(inputString) || string.IsNullOrWhiteSpace(inputString))
             {
                 return null;
             }
-            string[] keyValues = inputString.Split(keySeparaor);
+            string[] keyValues = inputString.Split(keySeparator);
             Dictionary<string, string> dict = new Dictionary<string, string>();
             for (int i = 0; i < keyValues.Length; i++)
             {
