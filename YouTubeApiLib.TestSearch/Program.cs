e971158c4d6c81eb55a4da5f3f7d1efae0737643
BlackRain <masaraksh86@mail.ru>
2024-12-10 15:03:34 +0700

    Дополнен тест "TestSearch"

--- YouTubeApiLib.TestSearch/Program.cs
+++ YouTubeApiLib.TestSearch/Program.cs
@@ -6,15 +6,27 @@ namespace YouTubeApiLib.TestSearch
 	{
 		static void Main(string[] args)
 		{
+			const string searchQuery = "coding train";
+			Console.WriteLine($"Searching for \"{searchQuery}\"... Please wait!");
+
 			YouTubeApi api = new YouTubeApi();
 			YouTubeApiV1SearchResults searchResults =
-				api.Search("coding train", null, YouTubeApiV1SearchResultFilters.Video);
-			if (searchResults != null)
+				api.Search(searchQuery, null, YouTubeApiV1SearchResultFilters.Video);
+			if (searchResults == null || searchResults.RawData == null)
 			{
-				// A raw info may be too long for console output!
-				//TODO: Write the mighty search results parsers.
+				Console.WriteLine("Error! The search result object is NULL!");
+			}
+			else if (searchResults.ErrorCode != 200)
+			{
+				Console.WriteLine($"There is error with code {searchResults.ErrorCode}!");
+			}
+			else
+			{
+				// Warning! A raw info may be too long for console output!
+				// TODO: Write the mighty search results parsers.
 				Console.WriteLine(searchResults.RawData.ToString());
 			}
+
 			Console.ReadLine();
 		}
 	}
